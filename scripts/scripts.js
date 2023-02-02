4a8b0b1aa4afdb9e1df10dcec841d3714df5f417
Miguel Roy Atienza <miguelroyatienza@gmail.com>
2023-02-02 14:25:01 +0800

    Reworked carousel, still has bug for looping

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,33 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const carousel = document.querySelector(".carousel-container");
+const track = document.querySelector(".track");
+const imageUrls = ["./images/carousel-1.jfif", "./images/carousel-2.jpg", "./images/carousel-3.jfif", "./images/carousel-4.jpg", "./images/carousel-5.jpg", "./images/carousel-6.jfif", "./images/carousel-7.jfif", "./images/carousel-8.jpg", "./images/carousel-9.jfif"];
+let index = 0;
+let currentPosition = 0;
+
+for (let i = 0; i < imageUrls.length; i++) {
+  let image = new Image();
+  image.src = imageUrls[i];
+  image.setAttribute("class", "img-fluid mx-auto d-block");
+  let child = document.createElement("div");
+  child.setAttribute("class", "item m-0 p-0 col-4");
+  track.appendChild(child);
+  child.appendChild(image);
+};
+
+prev.addEventListener("click", function() {
+  currentPosition += 33.33;
+  track.style.transform = "translateX(" + currentPosition + "%)";
+});
+
+next.addEventListener("click", function() {
+  currentPosition -= 33.33;
+  track.style.transform = "translateX(" + currentPosition + "%)";
+});
+
+//need to convert to vanilla JS
+
 const $dropdown = $(".dropdown");
 const $dropdownToggle = $(".dropdown-toggle");
 const $dropdownMenu = $(".dropdown-menu");
@@ -5,7 +35,7 @@ const showClass = "show";
 var totalItems = $('.carousel-item').length;
 var currentIndex = $('div.active').index() + 1;
 
-$(window).on("load resize", function() {
+$(window).on('load resize', function() {
   $('.pager-total').html(totalItems);
   if (this.matchMedia("(min-width: 768px)").matches) {
     $dropdown.hover(
@@ -25,9 +55,4 @@ $(window).on("load resize", function() {
   } else {
     $dropdown.off("mouseenter mouseleave");
   }
-});
-
-$('.carousel').on("slide.bs.carousel", function() {
-  currentIndex = $('div.active').index() + 1;
-  $('.pager-current').html(currentIndex);
 });
